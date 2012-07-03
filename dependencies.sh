0f8faae08f5832a16427e397c45a9ce82b617ca9
Richard Membarth <richard.membarth@cs.fau.de>
2012-07-03 15:30:52 +0200

    Add file with revision of dependencies.
    dependencies.sh lists the revisions of Clang/LLVM as well as libcxx and Polly in
    order to compile and install HIPAcc successfully.

---
+++ dependencies.sh
@@ -0,0 +1,10 @@
+clang_revision=402a6d5613b3df364ba252416d5a022dee95f0ba
+llvm_revision=84f2ae332f66b5e2ed66b3c3f404fb77b0c14c51
+libcxx_revision=518d150040a7830d5dbd23076fe2a2ded1093536
+polly_revision=87f1161faae67882508e04f939bb14c83669c72a
+
+clang_repository=http://llvm.org/git/clang.git
+llvm_repository=http://llvm.org/git/llvm.git
+libcxx_repository=http://llvm.org/git/libcxx.git
+polly_repository=http://llvm.org/git/polly.git
+
