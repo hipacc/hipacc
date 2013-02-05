2301e39198b71ddd95dcdf887c9ba7ba9d7fb9fc
Oliver Reiche <oliver.reiche@cs.fau.de>
2013-02-05 13:16:36 +0100

    Makefile: Removed copyright and license from RenderScript CMake find module
    --HG--
    extra : rebase_source : a765f987217839548e4dc0dd5bfa9dc81f22c387

--- cmake/modules/FindRenderScript.cmake
+++ cmake/modules/FindRenderScript.cmake
@@ -40,12 +40,6 @@
 #   NDK_LINK_LIBRARIES
 #     Specifies the NDK link libraries.
 #
-#
-# (c) Oliver Reiche
-#
-# This work is licensed under the Creative Commons Attribution 3.0 Unported
-# License. To view a copy of this license, visit
-# http://creativecommons.org/licenses/by/3.0/.
 
 INCLUDE (FindPackageHandleStandardArgs)
 
