09b0bb0a9b52e6b022fbe5c72a48bd8bb3893179
Richard Membarth <richard.membarth@cs.fau.de>
2013-02-12 19:53:00 +0100

    Remove trailing whitespace :%s/\s\+$//

--- lib/AST/ASTTranslate.cpp
+++ lib/AST/ASTTranslate.cpp
@@ -3,23 +3,23 @@
 // Copyright (c) 2012, Siemens AG
 // Copyright (c) 2010, ARM Limited
 // All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met: 
-// 
+// modification, are permitted provided that the following conditions are met:
+//
 // 1. Redistributions of source code must retain the above copyright notice, this
-//    list of conditions and the following disclaimer. 
+//    list of conditions and the following disclaimer.
 // 2. Redistributions in binary form must reproduce the above copyright notice,
 //    this list of conditions and the following disclaimer in the documentation
-//    and/or other materials provided with the distribution. 
-// 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+//    and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 // ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
-// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR 
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
 // ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
 // (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND 
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
 // ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
@@ -994,7 +994,7 @@ Stmt* ASTTranslate::Hipacc(Stmt *S) {
 
       // calculate multiple pixels per thread
       SmallVector<Stmt *, 16> pptBody;
-  
+
       if (p==0) {
         // initialize lid_y and gid_y
         lidYRef = tileVars.local_id_y;
@@ -1346,7 +1346,7 @@ Expr *ASTTranslate::VisitCallExpr(CallExpr *E) {
         Mask = Kernel->getMaskFromMapping(FD);
       }
       assert(Mask && "Could not find Mask Field Decl.");
-      
+
       // check if Mask is initialized with constants and only for the first
       // iteration (gidYRef is a DeclRefExpr and not a BinaryOperator)
       if (!Mask->isConstant() && isa<DeclRefExpr>(gidYRef) &&
@@ -1415,7 +1415,7 @@ Expr *ASTTranslate::VisitCallExpr(CallExpr *E) {
           Diags.Report(LE->getExprLoc(), DiagIDCapture) << cap.getCapturedVar();
         }
       }
-      
+
       // introduce temporary for holding the convolution result
       CompoundStmt *outerCompountStmt = curCompoundStmtVistor;
       std::stringstream LSST;
--- lib/AST/MemoryAccess.cpp
+++ lib/AST/MemoryAccess.cpp
@@ -3,23 +3,23 @@
 // Copyright (c) 2012, Siemens AG
 // Copyright (c) 2010, ARM Limited
 // All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met: 
-// 
+// modification, are permitted provided that the following conditions are met:
+//
 // 1. Redistributions of source code must retain the above copyright notice, this
-//    list of conditions and the following disclaimer. 
+//    list of conditions and the following disclaimer.
 // 2. Redistributions in binary form must reproduce the above copyright notice,
 //    this list of conditions and the following disclaimer in the documentation
-//    and/or other materials provided with the distribution. 
-// 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+//    and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 // ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
-// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR 
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
 // ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
 // (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND 
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
 // ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
@@ -444,7 +444,7 @@ Expr *ASTTranslate::accessMemTexAt(DeclRefExpr *LHS, HipaccAccessor *Acc,
     args.push_back(idx_y);
   }
 
-  return createFunctionCall(Ctx, texture_function, args); 
+  return createFunctionCall(Ctx, texture_function, args);
 }
 
 
@@ -472,7 +472,7 @@ Expr *ASTTranslate::accessMemImgAt(DeclRefExpr *LHS, HipaccAccessor *Acc,
     args.push_back(kernelSamplerRef);
     args.push_back(coord);
 
-    result = createFunctionCall(Ctx, image_function, args); 
+    result = createFunctionCall(Ctx, image_function, args);
     result = createExtVectorElementExpr(Ctx,
         Acc->getImage()->getPixelQualType(), result, "x");
   } else {
@@ -481,7 +481,7 @@ Expr *ASTTranslate::accessMemImgAt(DeclRefExpr *LHS, HipaccAccessor *Acc,
     if (image_function == builtins.getBuiltinFunction(OPENCLBIwrite_imagei)) {
       QT = simdTypes.getSIMDType(Ctx.IntTy, "int", SIMD4);
     } else if (image_function ==
-        builtins.getBuiltinFunction(OPENCLBIwrite_imageui)) { 
+        builtins.getBuiltinFunction(OPENCLBIwrite_imageui)) {
       QT = simdTypes.getSIMDType(Ctx.UnsignedIntTy, "uint", SIMD4);
     } else {
       QT = simdTypes.getSIMDType(Ctx.FloatTy, "float", SIMD4);
@@ -497,7 +497,7 @@ Expr *ASTTranslate::accessMemImgAt(DeclRefExpr *LHS, HipaccAccessor *Acc,
     args.push_back(coord);
     args.push_back(writeImageRHS);
 
-    result = createFunctionCall(Ctx, image_function, args); 
+    result = createFunctionCall(Ctx, image_function, args);
   }
 
   return result;
