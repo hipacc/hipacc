d59a4c4cc843c75e618abfca6a3272042f2fc169
Richard Membarth <richard.membarth@cs.fau.de>
2013-04-18 17:17:17 +0200

    Update dependencies.

--- dependencies.sh
+++ dependencies.sh
@@ -1,7 +1,7 @@
-clang_revision=50ea96332b5dae54cc724f924b343084cb9ea834
-llvm_revision=8849838965941919257b923f8603ca1bacd3fa46
-libcxx_revision=993248935c459c308379e14ccb09fa1c840d4a9c
-polly_revision=b7dcd05df598e8d69650db6cd0ba518182cf9568
+clang_revision=bd95745d0efb0d3c6e7cc2f946438a8b2c39c858
+llvm_revision=8a709208ede8eb387a2911c5ba11e5acf8cc69ae
+libcxx_revision=2062f377728e6c2a77842ca5b2c6428aab605ebc
+polly_revision=5f3abecad642a6646c53cbd39879091da87c3c37
 
 clang_repository=http://llvm.org/git/clang.git
 llvm_repository=http://llvm.org/git/llvm.git
