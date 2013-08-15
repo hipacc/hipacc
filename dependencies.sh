9b08dd70c3b8d597ef94cffa8d75b243bbfa2d48
Richard Membarth <richard.membarth@uni-saarland.de>
2013-08-15 11:38:24 +0200

    Upstream change: LambdaExpr.

--- dependencies.sh
+++ dependencies.sh
@@ -1,7 +1,7 @@
-clang_revision=03c55be552d224c787c3dd2e981b47a23b05cc2d
-llvm_revision=6db0dea22585a1b7577f24a97e60769e65e0a342
-libcxx_revision=8fc4f5a2510e709331c6fcc3ba401a36f98128fc
-polly_revision=30b716245d58b52fd99a8abc64851f1f1fa9076a
+clang_revision=233fbe1f56183878167a56be53425a6dd25ba334
+llvm_revision=20e4636c12cd1455d9f2cad46c81cbca6e0451dc
+libcxx_revision=de8fc6b2b146a49b78ec6949c2584fc7de274cfc
+polly_revision=5da43a19bd495f153d9a0f4333cc2136d31c93ce
 
 clang_repository=http://llvm.org/git/clang.git
 llvm_repository=http://llvm.org/git/llvm.git
