545a8db234a0ab1af88b081dae6f9bc8925b8b1a
Richard Membarth <richard.membarth@uni-saarland.de>
2013-11-06 01:37:12 +0100

    Configure and install Makefiles for test cases.

---
+++ tests/Makefile_test.cmake
@@ -0,0 +1,56 @@
+CC = clang++
+CC = g++
+
+MYFLAGS      ?= -D WIDTH=2048 -D HEIGHT=2048 -D SIZE_X=5 -D SIZE_Y=5 -D OpenCV
+CFLAGS        = $(MYFLAGS) -Wall -Wunused \
+                -I@DSL_INCLUDES@
+LDFLAGS       = -lm
+OFLAGS        = -O3
+
+ifeq ($(CC),clang++)
+    # use libc++ for clang++
+    CFLAGS   += -std=c++11 -stdlib=libc++ \
+                -I`@CLANG_EXECUTABLE@ -print-file-name=include` \
+                -I`@LLVM_CONFIG_EXECUTABLE@ --includedir` \
+                -I`@LLVM_CONFIG_EXECUTABLE@ --includedir`/c++/v1
+    LDFLAGS  += -L`@LLVM_CONFIG_EXECUTABLE@ --libdir` -lc++
+else
+    CFLAGS   += -std=c++0x
+    LDFLAGS  += -lstdc++
+endif
+
+
+BINARY = test
+BINDIR = bin
+OBJDIR = obj
+SOURCES = $(shell echo *.cpp)
+
+OBJS = $(SOURCES:%.cpp=$(OBJDIR)/%.o)
+BIN = $(BINDIR)/$(BINARY)
+
+
+all: $(BINARY)
+
+$(BINARY): $(OBJS) $(BINDIR)
+	$(CC) -o $(BINDIR)/$@ $(OBJS) $(LDFLAGS)
+
+$(OBJDIR)/%.o: %.cpp $(OBJDIR)
+	$(CC) $(CFLAGS) $(OFLAGS) -o $@ -c $<
+
+$(BINDIR):
+	mkdir bin
+
+$(OBJDIR):
+	mkdir obj
+
+
+clean:
+	rm -f $(BIN) $(OBJS)
+	@echo "all cleaned up!"
+
+distclean: clean
+	rm -rf $(BINDIR) $(OBJDIR)
+
+run: $(BINARY)
+	$(BIN)
+
