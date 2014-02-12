836a3214b5569eb64c5833456d1850ca5460a425
Oliver Reiche <oliver.reiche@cs.fau.de>
2014-02-12 18:18:51 +0100

    CMake: Only parse git origin if multiple remotes specified
    --HG--
    extra : rebase_source : 8a44bac392b804d96eaa93098876bf79915bd252

--- cmake/scripts/get_repository_path.sh
+++ cmake/scripts/get_repository_path.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -d .git ]; then
-    git remote -v | grep 'fetch' | awk '{ print $2 }'
+    git remote -v | grep 'origin.*fetch' | awk '{ print $2 }'
 else
     echo http://sourceforge.net/projects/hipacc/files/latest/download
 fi
