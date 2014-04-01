65f40ca9f09ffa0b50feeee8a4ce7ba2258303f5
Richard Membarth <richard.membarth@uni-saarland.de>
2014-03-31 21:43:17 -0700

    Add compiler-rt as dependency.

--- dependencies.sh
+++ dependencies.sh
@@ -1,10 +1,12 @@
 clang_revision=release_34
 llvm_revision=release_34
+compiler_rt_revision=release_34
 libcxx_revision=release_34
 polly_revision=release_34
 
 clang_repository=http://llvm.org/git/clang.git
 llvm_repository=http://llvm.org/git/llvm.git
+compiler_rt_repository=http://llvm.org/git/compiler-rt.git
 libcxx_repository=http://llvm.org/git/libcxx.git
 polly_repository=http://llvm.org/git/polly.git
 
