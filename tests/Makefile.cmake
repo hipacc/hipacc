199c05ed6d49c9eed7a1ae21d33ddbe6d74f2435
Richard Membarth <richard.membarth@uni-saarland.de>
2014-09-10 12:29:37 +0200

    Use -Xcompiler for nvcc only.

--- tests/Makefile.cmake
+++ tests/Makefile.cmake
@@ -17,8 +17,8 @@ NVCC_FLAGS      = -gencode=arch=compute_$(GPU_ARCH),code=\"sm_$(GPU_ARCH),comput
                   -Xptxas -v @NVCC_COMP@ #-keep
 OFLAGS          = -O3
 
-CC_CC           = @CMAKE_CXX_COMPILER@ -std=c++11 -Xcompiler -Wall -Xcompiler -Wunused
-CU_CC           = @NVCC@ $(NVCC_FLAGS) -Wall -Wunused
+CC_CC           = @CMAKE_CXX_COMPILER@ -std=c++11 -Wall -Wunused
+CU_CC           = @NVCC@ $(NVCC_FLAGS) -Xcompiler -Wall -Xcompiler -Wunused
 CC_LINK         = -lm -ldl -lstdc++ @TIME_LINK@
 CU_LINK         = $(CC_LINK) @CUDA_LINK@
 # OpenCL specific configuration
