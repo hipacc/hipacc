hipacc/hipacc

commit 95152a17307a5f89b4758ee1a2c853d0c3576d91

Make use of C++11 features & cleanup:
- auto
- range-based for loops
- ArrayRef
- unsigned int -> unsigned
- cleanup

lib/AST/MemoryAccess.cpp
@@ -150,12 +150,10 @@ Expr *ASTTranslate::accessMem(DeclRefExpr *LHS, HipaccAccessor *Acc,
         default: break;
         case TARGET_Renderscript: {
             bool isGlobalAllocation = false;
-            for (size_t i=0; i<Kernel->getKernelClass()->getNumArgs(); ++i) {
-              if (Kernel->getKernelClass()->getArguments()[0].name.compare(
-                    LHS->getNameInfo().getAsString()) == 0) {
-                isGlobalAllocation = true;
-                break;
-              }
+            if (Kernel->getKernelClass()->getArguments()[0].name.compare(
+                  LHS->getNameInfo().getAsString()) == 0) {
+              isGlobalAllocation = true;
+              break;
             }
             if (!isGlobalAllocation) {
               // access allocation by using local pointer type kernel argument
@@ -187,14 +185,14 @@ Expr *ASTTranslate::accessMem(DeclRefExpr *LHS, HipaccAccessor *Acc,
           return accessMemAllocAt(LHS, memAcc, idx_x, idx_y);
       }
     case READ_WRITE: {
-      unsigned int DiagIDRW = Diags.getCustomDiagID(DiagnosticsEngine::Error,
+      unsigned DiagIDRW = Diags.getCustomDiagID(DiagnosticsEngine::Error,
           "Reading and writing to Image '%0' in kernel '%1' is not supported.");
       Diags.Report(DiagIDRW) << LHS->getNameInfo().getAsString()
                              << KernelClass->getName();
       exit(EXIT_FAILURE); }
     default:
     case UNDEFINED: {
-      unsigned int DiagIDU = Diags.getCustomDiagID(DiagnosticsEngine::Error,
+      unsigned DiagIDU = Diags.getCustomDiagID(DiagnosticsEngine::Error,
           "Memory access pattern for Image '%0' in kernel '%1' could not be analyzed.");
       Diags.Report(DiagIDU) << LHS->getNameInfo().getAsString()
                             << KernelClass->getName();
@@ -752,9 +750,8 @@ void ASTTranslate::stageLineToSharedMemory(ParmVarDecl *PVD,
         bhStmts, bhCStmt);
 
     // add border handling statements to stageBody
-    for (size_t i=0, e=bhStmts.size(); i!=e; ++i) {
-      stageBody.push_back(bhStmts[i]);
-    }
+    for (auto stmt : bhStmts)
+      stageBody.push_back(stmt);
   } else {
     RHS = accessMem(paramDRE, Acc, READ_ONLY, global_offset_x, global_offset_y);
   }
@@ -767,11 +764,9 @@ void ASTTranslate::stageLineToSharedMemory(ParmVarDecl *PVD,
 // stage iteration p to shared memory
 void ASTTranslate::stageIterationToSharedMemory(SmallVector<Stmt *, 16>
     &stageBody, int p) {
-  for (auto I=kernelDecl->param_begin(), N=kernelDecl->param_end(); I!=N; ++I) {
-    ParmVarDecl *PVD = *I;
-
-    if (KernelDeclMapShared[PVD]) {
-      HipaccAccessor *Acc = KernelDeclMapAcc[PVD];
+  for (auto param : kernelDecl->params()) {
+    if (KernelDeclMapShared[param]) {
+      HipaccAccessor *Acc = KernelDeclMapAcc[param];
 
       // check if the bottom apron has to be fetched
       if (p>=(int)Kernel->getPixelsPerThread()) {
@@ -819,7 +814,7 @@ void ASTTranslate::stageIterationToSharedMemory(SmallVector<Stmt *, 16>
               BO_Sub, Ctx.IntTy);
         }
 
-        stageLineToSharedMemory(PVD, stageBody, local_offset_x, nullptr,
+        stageLineToSharedMemory(param, stageBody, local_offset_x, nullptr,
             global_offset_x, global_offset_y);
       }
     }
@@ -830,11 +825,9 @@ void ASTTranslate::stageIterationToSharedMemory(SmallVector<Stmt *, 16>
 // stage data to shared memory for exploration
 void ASTTranslate::stageIterationToSharedMemoryExploration(SmallVector<Stmt *,
     16> &stageBody) {
-  for (auto I=kernelDecl->param_begin(), N=kernelDecl->param_end(); I!=N; ++I) {
-    ParmVarDecl *PVD = *I;
-
-    if (KernelDeclMapShared[PVD]) {
-      HipaccAccessor *Acc = KernelDeclMapAcc[PVD];
+  for (auto param : kernelDecl->params()) {
+    if (KernelDeclMapShared[param]) {
+      HipaccAccessor *Acc = KernelDeclMapAcc[param];
 
       Expr *global_offset_x = nullptr, *global_offset_y = nullptr;
       Expr *SX2;
@@ -882,7 +875,7 @@ void ASTTranslate::stageIterationToSharedMemoryExploration(SmallVector<Stmt *,
               BO_Sub, Ctx.IntTy);
         }
 
-        stageLineToSharedMemory(PVD, stageIter, local_offset_x,
+        stageLineToSharedMemory(param, stageIter, local_offset_x,
             createBinaryOperator(Ctx, iter_ref, tileVars.local_size_y, BO_Mul,
               Ctx.IntTy), global_offset_x, global_offset_y);
       }

compiler/hipacc.cpp
@@ -48,6 +48,10 @@ static void LLVMErrorHandler(void *userData, const std::string &message, bool
   // particular that we remove files registered with RemoveFileOnSignal.
   llvm::sys::RunInterruptHandlers();
 
+  // Run the interrupt handlers to make sure any special cleanups get done, in
+  // particular that we remove files registered with RemoveFileOnSignal.
+  llvm::sys::RunInterruptHandlers();
+
   // We cannot recover from llvm errors.  When reporting a fatal error, exit
   // with status 70 to generate crash diagnostics.  For BSD systems this is
   // defined as an internal software error.  Otherwise, exit with status 1.

include/hipacc/AST/ASTNode.h
@@ -48,7 +48,7 @@ FunctionDecl *createFunctionDecl(ASTContext &Ctx, DeclContext *DC, StringRef
 
 // creates a function call AST node
 CallExpr *createFunctionCall(ASTContext &Ctx, FunctionDecl *FD,
-    SmallVector<Expr *, 16> Expr);
+    ArrayRef<Expr *> Expr);
 DeclRefExpr *createDeclRefExpr(ASTContext &Ctx, ValueDecl *decl);
 
 
@@ -57,16 +57,15 @@ DeclStmt *createDeclStmt(ASTContext &Ctx, Decl *VD);
 VarDecl *createVarDecl(ASTContext &Ctx, DeclContext *DC, StringRef Name,
     QualType T, Expr *init=nullptr);
 RecordDecl *createRecordDecl(ASTContext &Ctx, DeclContext *DC, StringRef Name,
-    TagDecl::TagKind TK, unsigned int numDecls, QualType *declTypes, StringRef
-    *declNames);
+    TagDecl::TagKind TK, ArrayRef<QualType> declTypes, ArrayRef<StringRef>
+    declNames);
 
 // create a member expression AST node
 MemberExpr *createMemberExpr(ASTContext &Ctx, Expr *base, bool isArrow,
     ValueDecl *memberdecl, QualType T);
 
 // creates a compound statement AST node representing multiple statements
-CompoundStmt *createCompoundStmt(ASTContext &Ctx, SmallVector<Stmt *, 16>
-    Stmts);
+CompoundStmt *createCompoundStmt(ASTContext &Ctx, ArrayRef<Stmt *> Stmts);
 
 // creates a return statement AST node
 ReturnStmt *createReturnStmt(ASTContext &Ctx, Expr *E);

include/hipacc/AST/ASTTranslate.h
@@ -57,12 +57,12 @@ namespace clang {
 namespace hipacc {
 typedef union border_variant {
   struct {
-    unsigned int left   : 1;
-    unsigned int right  : 1;
-    unsigned int top    : 1;
-    unsigned int bottom : 1;
+    unsigned left   : 1;
+    unsigned right  : 1;
+    unsigned top    : 1;
+    unsigned bottom : 1;
   } borders;
-  unsigned int borderVal;
+  unsigned borderVal;
   border_variant() : borderVal(0) {}
 } border_variant;
 
@@ -85,7 +85,7 @@ class ASTTranslate : public StmtVisitor<ASTTranslate, Stmt *> {
     bool emitEstimation;
 
     // "global variables"
-    unsigned int literalCount;
+    unsigned literalCount;
     SmallVector<FunctionDecl *, 16> cloneFuns;
     SmallVector<Stmt *, 16> preStmts, postStmts;
     SmallVector<CompoundStmt *, 16> preCStmt, postCStmt;
@@ -343,7 +343,7 @@ class ASTTranslate : public StmtVisitor<ASTTranslate, Stmt *> {
       lidYRef(nullptr),
       gidYRef(nullptr) {
         // get 'hipacc' namespace context for lookups
-        for (DeclContext::lookup_result Lookup =
+        for (auto Lookup =
             Ctx.getTranslationUnitDecl()->lookup(&Ctx.Idents.get("hipacc"));
             !Lookup.empty(); Lookup=Lookup.slice(1)) {
           hipaccNS = cast_or_null<NamespaceDecl>(Lookup.front());
@@ -352,15 +352,14 @@ class ASTTranslate : public StmtVisitor<ASTTranslate, Stmt *> {
         assert(hipaccNS && "could not lookup 'hipacc' namespace");
 
         // get 'hipacc::' namespace context for lookups
-        for (DeclContext::lookup_result Lookup =
-            hipaccNS->lookup(&Ctx.Idents.get("math"));
+        for (auto Lookup = hipaccNS->lookup(&Ctx.Idents.get("math"));
             !Lookup.empty(); Lookup=Lookup.slice(1)) {
           hipaccMathNS = cast_or_null<NamespaceDecl>(Lookup.front());
           if (hipaccMathNS) break;
         }
         assert(hipaccMathNS && "could not lookup 'hipacc::math' namespace");
 
-        // typedef unsigned int sampler_t;
+        // typedef unsigned sampler_t;
         TypeSourceInfo *TInfosampler =
           Ctx.getTrivialTypeSourceInfo(Ctx.UnsignedIntTy);
         samplerTy = TypedefDecl::Create(Ctx, Ctx.getTranslationUnitDecl(),

include/hipacc/Config/CompilerOptions.h
@@ -246,18 +246,13 @@ class CompilerOptions {
 
     std::string getTargetPrefix() {
       switch (target_code) {
-        case TARGET_C:
-          return "cc";
-        case TARGET_CUDA:
-          return "cu";
+        case TARGET_C:            return "cc";
+        case TARGET_CUDA:         return "cu";
         case TARGET_OpenCLACC:
         case TARGET_OpenCLCPU:
-        case TARGET_OpenCLGPU:
-          return "cl";
-        case TARGET_Renderscript:
-          return "rs";
-        case TARGET_Filterscript:
-          return "fs";
+        case TARGET_OpenCLGPU:    return "cl";
+        case TARGET_Renderscript: return "rs";
+        case TARGET_Filterscript: return "fs";
       }
     }
 
@@ -265,27 +260,13 @@ class CompilerOptions {
       llvm::errs() << "HIPACC compiler configuration summary: \n";
       llvm::errs() << "  Generating target code for '";
       switch (target_code) {
-        case TARGET_CUDA:
-          llvm::errs() << "CUDA";
-          break;
-        case TARGET_OpenCLACC:
-          llvm::errs() << "OpenCL (ACC)";
-          break;
-        case TARGET_OpenCLCPU:
-          llvm::errs() << "OpenCL (CPU)";
-          break;
-        case TARGET_OpenCLGPU:
-          llvm::errs() << "OpenCL (GPU)";
-          break;
-        case TARGET_Renderscript:
-          llvm::errs() << "Renderscript";
-          break;
-        case TARGET_Filterscript:
-          llvm::errs() << "Filterscript";
-          break;
-        case TARGET_C:
-          llvm::errs() << "C/C++";
-          break;
+        case TARGET_C:            llvm::errs() << "C/C++";        break;
+        case TARGET_CUDA:         llvm::errs() << "CUDA";         break;
+        case TARGET_OpenCLACC:    llvm::errs() << "OpenCL (ACC)"; break;
+        case TARGET_OpenCLCPU:    llvm::errs() << "OpenCL (CPU)"; break;
+        case TARGET_OpenCLGPU:    llvm::errs() << "OpenCL (GPU)"; break;
+        case TARGET_Renderscript: llvm::errs() << "Renderscript"; break;
+        case TARGET_Filterscript: llvm::errs() << "Filterscript"; break;
       }
       llvm::errs() << "' language.\n";
       llvm::errs() << "  Target device is '" << target_device << "'";
@@ -306,10 +287,10 @@ class CompilerOptions {
       getOptionAsString(texture_memory);
       switch (texture_memory_type) {
         case NoTexture: break;
-        case Linear1D: llvm::errs() << ": Linear1D"; break;
-        case Linear2D: llvm::errs() << ": Linear2D"; break;
-        case Array2D: llvm::errs() << ": Array2D"; break;
-        case Ldg: llvm::errs() << ": Ldg"; break;
+        case Linear1D:  llvm::errs() << ": Linear1D"; break;
+        case Linear2D:  llvm::errs() << ": Linear2D"; break;
+        case Array2D:   llvm::errs() << ": Array2D";  break;
+        case Ldg:       llvm::errs() << ": Ldg";      break;
       }
       llvm::errs() << "\n  Usage of local memory reading from images: ";
       getOptionAsString(local_memory);

include/hipacc/DSL/ClassRepresentation.h
@@ -91,7 +91,7 @@ enum InterpolationMode {
 // common base class for images, masks and pyramids
 class HipaccSize {
   protected:
-    unsigned int size_x, size_y;
+    unsigned size_x, size_y;
     std::string size_x_str, size_y_str;
 
   public:
@@ -100,22 +100,22 @@ class HipaccSize {
       size_x_str(), size_y_str()
     {}
 
-    void setSizeX(unsigned int x) {
+    void setSizeX(unsigned x) {
       std::string Str;
       llvm::raw_string_ostream SS(Str);
       SS << x;
       size_x_str = SS.str();
       size_x = x;
     }
-    void setSizeY(unsigned int y) {
+    void setSizeY(unsigned y) {
       std::string Str;
       llvm::raw_string_ostream SS(Str);
       SS << y;
       size_y_str = SS.str();
       size_y = y;
     }
-    unsigned int getSizeX() { return size_x; }
-    unsigned int getSizeY() { return size_y; }
+    unsigned getSizeX() { return size_x; }
+    unsigned getSizeY() { return size_y; }
     std::string getSizeXStr() {
       assert(!size_x_str.empty());
       return size_x_str;
@@ -158,7 +158,7 @@ class HipaccImage : public HipaccMemory {
       Ctx(Ctx)
     {}
 
-    unsigned int getPixelSize() { return Ctx.getTypeSize(type)/8; }
+    unsigned getPixelSize() { return Ctx.getTypeSize(type)/8; }
     std::string getTextureType();
     std::string getImageReadFunction();
 };
@@ -244,8 +244,8 @@ class HipaccAccessor {
     HipaccBoundaryCondition *getBC() { return bc; }
     InterpolationMode getInterpolation() { return interpolation; }
     HipaccImage *getImage() { return bc->getImage(); }
-    unsigned int getSizeX() { return bc->getSizeX(); }
-    unsigned int getSizeY() { return bc->getSizeY(); }
+    unsigned getSizeX() { return bc->getSizeX(); }
+    unsigned getSizeY() { return bc->getSizeY(); }
     std::string getSizeXStr() { return bc->getSizeXStr(); }
     std::string getSizeYStr() { return bc->getSizeYStr(); }
     DeclRefExpr *getWidthDecl() { return widthDecl; }
@@ -346,38 +346,38 @@ class HipaccMask : public HipaccMemory {
       return dyn_cast<InitListExpr>(init_list->getInit(y))->getInit(x);
     }
     void addKernel(HipaccKernel *K) { kernels.push_back(K); }
-    SmallVector<HipaccKernel *, 16> &getKernels() { return kernels; }
+    ArrayRef<HipaccKernel *> getKernels() { return kernels; }
     void setHostMemName(std::string name) { hostMemName = name; }
     std::string getHostMemName() { return hostMemName; }
-    void setSizeX(unsigned int x) {
+    void setSizeX(unsigned x) {
       HipaccMemory::setSizeX(x);
       if (isDomain()) { setDomainSize(size_x*size_y); }
     }
-    void setSizeY(unsigned int y) {
+    void setSizeY(unsigned y) {
       HipaccMemory::setSizeY(y);
       if (isDomain()) { setDomainSize(size_x*size_y); }
     }
-    void setDomainSize(unsigned int size) {
+    void setDomainSize(unsigned size) {
       if (domain_space) {
         delete[] domain_space;
         domain_space = nullptr;
       }
       if (size > 0) {
         domain_space = new bool[size];
-        for (unsigned int i = 0; i < size; ++i) {
+        for (size_t i = 0; i < size; ++i) {
           domain_space[i] = true;
         }
       }
     }
-    void setDomainDefined(unsigned int pos, bool def) {
+    void setDomainDefined(unsigned pos, bool def) {
       if (domain_space) { domain_space[pos] = def; }
     }
-    void setDomainDefined(unsigned int x, unsigned int y, bool def) {
-      unsigned int pos = (y * size_x) + x;
+    void setDomainDefined(unsigned x, unsigned y, bool def) {
+      unsigned pos = (y * size_x) + x;
       setDomainDefined(pos, def);
     }
-    bool isDomainDefined(unsigned int x, unsigned int y) {
-      unsigned int pos = (y * size_x) + x;
+    bool isDomainDefined(unsigned x, unsigned y) {
+      unsigned pos = (y * size_x) + x;
       return domain_space && domain_space[pos];
     }
     void setCopyMask(HipaccMask *mask) {
@@ -476,13 +476,9 @@ class HipaccKernelClass {
       arguments.push_back(a);
     }
 
-    unsigned int getNumArgs() { return arguments.size(); }
-    unsigned int getNumImages() { return imgFields.size(); }
-    unsigned int getNumMasks() { return maskFields.size(); }
-
-    SmallVector<argumentInfo, 16> &getArguments() { return arguments; }
-    SmallVector<FieldDecl *, 16> &getImgFields() { return imgFields; }
-    SmallVector<FieldDecl *, 16> &getMaskFields() { return maskFields; }
+    ArrayRef<argumentInfo> getArguments() { return arguments; }
+    ArrayRef<FieldDecl *>  getImgFields() { return imgFields; }
+    ArrayRef<FieldDecl *>  getMaskFields() { return maskFields; }
 
     friend class HipaccKernel;
 };
@@ -587,7 +583,7 @@ class HipaccKernelFeatures : public HipaccDevice {
       return vectorization;
     }
 
-    unsigned int getPixelsPerThread() {
+    unsigned getPixelsPerThread() {
       return pixels_per_thread[KC->getKernelType()];
     }
 };
@@ -601,7 +597,7 @@ class HipaccKernel : public HipaccKernelFeatures {
     std::string kernelName, reduceName;
     std::string fileName;
     std::string reduceStr, infoStr;
-    unsigned int infoStrCnt;
+    unsigned infoStrCnt;
     HipaccIterationSpace *iterationSpace;
     std::map<FieldDecl *, HipaccAccessor *> imgMap;
     std::map<FieldDecl *, HipaccMask *> maskMap;
@@ -615,11 +611,11 @@ class HipaccKernel : public HipaccKernelFeatures {
     SmallVector<FieldDecl *, 16> deviceArgFields;
     SmallVector<FunctionDecl *, 16> deviceFuncs;
     std::set<std::string> usedVars;
-    unsigned int max_threads_for_kernel;
-    unsigned int max_size_x, max_size_y;
-    unsigned int max_size_x_undef, max_size_y_undef;
-    unsigned int num_threads_x, num_threads_y;
-    unsigned int num_reg, num_lmem, num_smem, num_cmem;
+    unsigned max_threads_for_kernel;
+    unsigned max_size_x, max_size_y;
+    unsigned max_size_x_undef, max_size_y_undef;
+    unsigned num_threads_x, num_threads_y;
+    unsigned num_reg, num_lmem, num_smem, num_cmem;
 
     void calcSizes();
     void calcConfig();
@@ -627,7 +623,7 @@ class HipaccKernel : public HipaccKernelFeatures {
     void addParam(QualType QT1, QualType QT2, QualType QT3, std::string typeC,
         std::string typeO, std::string name, FieldDecl *fd);
     void createHostArgInfo(ArrayRef<Expr *> hostArgs, std::string &hostLiterals,
-        unsigned int &literalCount);
+        unsigned &literalCount);
 
   public:
     HipaccKernel(ASTContext &Ctx, VarDecl *VD, HipaccKernelClass *KC,
@@ -695,10 +691,8 @@ class HipaccKernel : public HipaccKernelFeatures {
     void resetUsed() {
       usedVars.clear();
       deviceFuncs.clear();
-      for (auto iter = imgMap.begin(), eiter=imgMap.end(); iter!=eiter; ++iter)
-      {
-        iter->second->resetDecls();
-      }
+      for (auto map : imgMap)
+        map.second->resetDecls();
     }
     bool getUsed(std::string name) {
       if (usedVars.find(name) != usedVars.end()) return true;
@@ -706,12 +700,8 @@ class HipaccKernel : public HipaccKernelFeatures {
     }
 
     // keep track of functions called within kernel
-    void addFunctionCall(FunctionDecl *FD) {
-      deviceFuncs.push_back(FD);
-    }
-    ArrayRef<FunctionDecl *> getFunctionCalls() {
-      return ArrayRef<FunctionDecl*>(deviceFuncs.data(), deviceFuncs.size());
-    }
+    void addFunctionCall(FunctionDecl *FD) { deviceFuncs.push_back(FD); }
+    ArrayRef<FunctionDecl *> getFunctionCalls() { return deviceFuncs; }
 
     void setIterationSpace(HipaccIterationSpace *IS) {
       iterationSpace = IS;
@@ -741,48 +731,43 @@ class HipaccKernel : public HipaccKernelFeatures {
       else return iter->second;
     }
 
-    unsigned int getNumArgs() {
-      createArgInfo();
-      return deviceArgNames.size();
-    }
     ArrayRef<QualType> getArgTypes(ASTContext &Ctx, TargetCode target_code) {
       createArgInfo();
 
       switch (target_code) {
         case TARGET_C:
-          return ArrayRef<QualType>(argTypesC.data(), argTypesC.size());
+          return argTypesC;
         case TARGET_CUDA:
-          return ArrayRef<QualType>(argTypesCUDA.data(), argTypesCUDA.size());
+          return argTypesCUDA;
         case TARGET_OpenCLACC:
         case TARGET_OpenCLCPU:
         case TARGET_OpenCLGPU:
         case TARGET_Renderscript:
         case TARGET_Filterscript:
-          return ArrayRef<QualType>(argTypesOpenCL.data(),
-              argTypesOpenCL.size());
+          return argTypesOpenCL;
       }
     }
-    std::string *getArgTypeNames() {
+    ArrayRef<std::string>getArgTypeNames() {
       createArgInfo();
-      if (options.emitOpenCL()) return argTypeNamesOpenCL.data();
-      else return argTypeNames.data();
+      if (options.emitOpenCL()) return argTypeNamesOpenCL;
+      else return argTypeNames;
     }
     ArrayRef<std::string> getDeviceArgNames() {
       createArgInfo();
-      return makeArrayRef(deviceArgNames);
+      return deviceArgNames;
     }
     void setHostArgNames(ArrayRef<Expr *>hostArgs, std::string
-        &hostLiterals, unsigned int &literalCount) {
+        &hostLiterals, unsigned &literalCount) {
       createArgInfo();
       createHostArgInfo(hostArgs, hostLiterals, literalCount);
     }
-    std::string *getHostArgNames() {
+    ArrayRef<std::string>getHostArgNames() {
       assert(hostArgNames.size() && "host argument names not set");
-      return hostArgNames.data();
+      return hostArgNames;
     }
     ArrayRef<FieldDecl *>getDeviceArgFields() {
       createArgInfo();
-      return makeArrayRef(deviceArgFields);
+      return deviceArgFields;
     }
 
     void setResourceUsage(int reg, int lmem, int smem, int cmem) {
@@ -818,35 +803,34 @@ class HipaccKernel : public HipaccKernelFeatures {
       llvm::errs() << "  Vectorization: " << vectorize() << "\n";
       llvm::errs() << "  Pixels per thread: " << getPixelsPerThread() << "\n";
 
-      for (auto iter = memMap.begin(), eiter=memMap.end(); iter!=eiter; ++iter)
-      {
-        llvm::errs() << "  Image '" << iter->first->getName() << "': ";
-        if (iter->second & Global) llvm::errs() << "global ";
-        if (iter->second & Constant) llvm::errs() << "constant ";
-        if (iter->second & Texture) llvm::errs() << "texture ";
-        if (iter->second & Local) llvm::errs() << "local ";
+      for (auto map : memMap) {
+        llvm::errs() << "  Image '" << map.first->getName() << "': ";
+        if (map.second & Global) llvm::errs() << "global ";
+        if (map.second & Constant) llvm::errs() << "constant ";
+        if (map.second & Texture) llvm::errs() << "texture ";
+        if (map.second & Local) llvm::errs() << "local ";
         llvm::errs() << "\n";
       }
     }
 
-    unsigned int getMaxThreadsForKernel() { return max_threads_for_kernel; }
-    unsigned int getMaxThreadsPerBlock() { return max_threads_per_block; }
-    unsigned int getMaxTotalSharedMemory() { return max_total_shared_memory; }
-    unsigned int getWarpSize() { return max_threads_per_warp; }
-    unsigned int getMaxSizeX() { return max_size_x<=1?0:max_size_x>>1; }
-    unsigned int getMaxSizeY() { return max_size_y<=1?0:max_size_y>>1; }
-    unsigned int getMaxSizeXUndef() {
+    unsigned getMaxThreadsForKernel() { return max_threads_for_kernel; }
+    unsigned getMaxThreadsPerBlock() { return max_threads_per_block; }
+    unsigned getMaxTotalSharedMemory() { return max_total_shared_memory; }
+    unsigned getWarpSize() { return max_threads_per_warp; }
+    unsigned getMaxSizeX() { return max_size_x<=1?0:max_size_x>>1; }
+    unsigned getMaxSizeY() { return max_size_y<=1?0:max_size_y>>1; }
+    unsigned getMaxSizeXUndef() {
       return max_size_x_undef<=1?0:max_size_x_undef>>1;
     }
-    unsigned int getMaxSizeYUndef() {
+    unsigned getMaxSizeYUndef() {
       return max_size_y_undef<=1?0:max_size_y_undef>>1;
     }
-    unsigned int getNumThreadsX() { return num_threads_x; }
-    unsigned int getNumThreadsY() { return num_threads_y; }
-    unsigned int getNumThreadsReduce() {
+    unsigned getNumThreadsX() { return num_threads_x; }
+    unsigned getNumThreadsY() { return num_threads_y; }
+    unsigned getNumThreadsReduce() {
       return default_num_threads_x*default_num_threads_y;
     }
-    unsigned int getPixelsPerThreadReduce() {
+    unsigned getPixelsPerThreadReduce() {
       return pixels_per_thread[GlobalOperator];
     }
 };

include/hipacc/Device/Builtins.h
@@ -71,7 +71,7 @@ class Context {
   private:
     ASTContext &Ctx;
     bool initialized;
-    const Info &getRecord(unsigned int ID) const;
+    const Info &getRecord(unsigned ID) const;
 
   public:
     Context(ASTContext &Ctx) :
@@ -79,31 +79,26 @@ class Context {
       initialized(false)
     {}
 
-    QualType getBuiltinType(unsigned int Id) const;
+    QualType getBuiltinType(unsigned Id) const;
     QualType getBuiltinType(const char *TypeStr) const;
     std::string EncodeTypeIntoStr(QualType QT, const ASTContext &Ctx);
 
     void InitializeBuiltins();
-    FunctionDecl *CreateBuiltin(unsigned int bid);
+    FunctionDecl *CreateBuiltin(unsigned bid);
     FunctionDecl *CreateBuiltin(QualType R, const char *Name);
 
     void getBuiltinNames(TargetCode target, SmallVectorImpl<const char *>
         &Names);
 
-    FunctionDecl *getBuiltinFunction(unsigned int ID) const {
+    FunctionDecl *getBuiltinFunction(unsigned ID) const {
       return getRecord(ID-FirstBuiltin).FD;
     }
 
     FunctionDecl *getBuiltinFunction(StringRef Name, QualType QT, TargetCode
         target) const;
 
-    const char *getName(unsigned int ID) const {
-      return getRecord(ID).Name;
-    }
-
-    const char *getTypeString(unsigned int ID) const {
-      return getRecord(ID).Type;
-    }
+    const char *getName(unsigned ID) const { return getRecord(ID).Name; }
+    const char *getTypeString(unsigned ID) const { return getRecord(ID).Type; }
 };
 }
 } // end namespace hipacc

commit cb4a2acccc16b4179dc38eda48b2595aa964186b

Fis bug introduced by 95152a1
- spotted by clang-analyzer

lib/AST/MemoryAccess.cpp
@@ -147,16 +147,12 @@ Expr *ASTTranslate::accessMem(DeclRefExpr *LHS, HipaccAccessor *Acc,
       switch (compilerOptions.getTargetLang()) {
         default: break;
         case Language::Renderscript: {
-            bool isGlobalAllocation = false;
             if (Kernel->getKernelClass()->getMembers()[0].name.compare(
-                  LHS->getNameInfo().getAsString()) == 0) {
-              isGlobalAllocation = true;
-              break;
-            }
-            if (!isGlobalAllocation) {
+                  LHS->getNameInfo().getAsString()) != 0) {
               // access allocation by using local pointer type kernel argument
               return accessMemAllocPtr(LHS);
             }
+            // fall through to READ_ONLY for global allocation
           }
           break;
         case Language::Filterscript: