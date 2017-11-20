d0465c4a43261d13be4f7bb414968d4d545dfef5
Richard Membarth <richard.membarth@uni-saarland.de>
2017-11-20 11:17:37 +0100

    Hard-code Android API used for compilation.

--- cmake/modules/FindRenderScript.cmake
+++ cmake/modules/FindRenderScript.cmake
@@ -2,14 +2,12 @@
 #
 # Customizable variables:
 #  NDK_DIR                              - NDK source directory (e.g. NDK_DIR=/opt/android/android-ndk-r14)
-#  ANDROID_API                          - Defines Android API level to use
 #
 # Once done this will define
 #  Renderscript_ndk_build_EXECUTABLE    - ndk-build executable
 #  Renderscript_FOUND                   - True if Renderscript is found
 
 set(NDK_DIR         $ENV{NDK_DIR}   CACHE PATH      "NDK source directory.")
-set(ANDROID_API     "19"            CACHE STRING    "Android API level.")
 
 find_program(Renderscript_ndk_build_EXECUTABLE ndk-build HINTS "${NDK_DIR}" DOC "NDK build executable")
 
